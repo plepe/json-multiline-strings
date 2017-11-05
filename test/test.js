00994d4cad7d5383c388d1cb2c376aa522a09e44
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-11-05 21:08:35 +0100

    improve tests

--- test/test.js
+++ test/test.js
@@ -13,8 +13,8 @@ describe('jsonMultilineStrings.split', function () {
 
 describe('jsonMultilineStrings.join', function () {
   it('simple', function () {
-    var input = { foo: 'bar', long: [ 'text with', 'several', 'line breaks' ] }
-    var expected = { foo: 'bar', long: 'text with\nseveral\nline breaks' }
+    var input = { foo: 'bar', long: [ 'text with', 'several', 'line breaks' ], nojoin: [ 'this no join', 1 ] }
+    var expected = { foo: 'bar', long: 'text with\nseveral\nline breaks', nojoin: [ 'this no join', 1 ] }
 
     assert.deepEqual(expected, jsonMultilineStrings.join(input))
   })
--- test/test.php
+++ test/test.php
@@ -18,8 +18,8 @@ class jsonMultilineStringsJoin_test extends TestCase
 {
     public function testjsonMultilineStringsJoin()
     {
-        $input = array( "foo" => "bar", "long" => array("text with", "several", "line breaks") );
-        $expected = array( "foo" => "bar", "long" => "text with\nseveral\nline breaks");
+        $input = array( "foo" => "bar", "long" => array("text with", "several", "line breaks"), "nojoin" => array( "this no join", 1 ) );
+        $expected = array( "foo" => "bar", "long" => "text with\nseveral\nline breaks", "nojoin" => array( "this no join", 1 ));
 
         $this->assertEquals($expected, jsonMultilineStringsJoin($input));
     }
