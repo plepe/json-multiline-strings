946e701c8a747a521c859fe84085e4471d5e7a77
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-11-05 09:28:57 +0100

    lint tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
+/* global describe, it */
 var assert = require('assert')
 var jsonMultilineStrings = require('../src/json-multiline-strings')
 
 describe('jsonMultilineStrings.split', function () {
   it('simple', function () {
-    var input = { foo: "bar", long: "text with\nseveral\nline breaks" }
-    var expected = { foo: "bar", long: [ "text with", "several", "line breaks" ] }
+    var input = { foo: 'bar', long: 'text with\nseveral\nline breaks' }
+    var expected = { foo: 'bar', long: [ 'text with', 'several', 'line breaks' ] }
 
     assert.deepEqual(expected, jsonMultilineStrings.split(input))
   })
@@ -12,8 +13,8 @@ describe('jsonMultilineStrings.split', function () {
 
 describe('jsonMultilineStrings.join', function () {
   it('simple', function () {
-    var input = { foo: "bar", long: [ "text with", "several", "line breaks" ] }
-    var expected = { foo: "bar", long: "text with\nseveral\nline breaks" }
+    var input = { foo: 'bar', long: [ 'text with', 'several', 'line breaks' ] }
+    var expected = { foo: 'bar', long: 'text with\nseveral\nline breaks' }
 
     assert.deepEqual(expected, jsonMultilineStrings.join(input))
   })
--- test/test.php
+++ test/test.php
@@ -1,21 +1,26 @@
 <?php
 use PHPUnit\Framework\TestCase;
+
 require("src/json-multiline-strings.php");
 
-class jsonMultilineStringsSplit_test extends TestCase {
-  public function testjsonMultilineStringsSplit () {
-    $input = array( "foo" => "bar", "long" => "text with\nseveral\nline breaks");
-    $expected = array( "foo" => "bar", "long" => array("text with", "several", "line breaks") );
+class jsonMultilineStringsSplit_test extends TestCase
+{
+    public function testjsonMultilineStringsSplit()
+    {
+        $input = array( "foo" => "bar", "long" => "text with\nseveral\nline breaks");
+        $expected = array( "foo" => "bar", "long" => array("text with", "several", "line breaks") );
 
-    $this->assertEquals($expected, jsonMultilineStringsSplit($input));
-  }
+        $this->assertEquals($expected, jsonMultilineStringsSplit($input));
+    }
 }
 
-class jsonMultilineStringsJoin_test extends TestCase {
-  public function testjsonMultilineStringsJoin () {
-    $input = array( "foo" => "bar", "long" => array("text with", "several", "line breaks") );
-    $expected = array( "foo" => "bar", "long" => "text with\nseveral\nline breaks");
+class jsonMultilineStringsJoin_test extends TestCase
+{
+    public function testjsonMultilineStringsJoin()
+    {
+        $input = array( "foo" => "bar", "long" => array("text with", "several", "line breaks") );
+        $expected = array( "foo" => "bar", "long" => "text with\nseveral\nline breaks");
 
-    $this->assertEquals($expected, jsonMultilineStringsJoin($input));
-  }
+        $this->assertEquals($expected, jsonMultilineStringsJoin($input));
+    }
 }
