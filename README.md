0e24ffd6a241b5a3b71470fb90816223503ebb8b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-11-05 08:59:58 +0100

    lint

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # json-multiline-strings
 One shortcoming of JSON is the lack of multiline support. This module will split/join multiline strings to string arrays which is more readable.
+
+# INSTALL
+## Development
+```sh
+git clone https://github.com/plepe/json-multiline-strings.git
+cd json-multiline-strings
+npm install
+composer install
+```
--- src/json-multiline-strings.php
+++ src/json-multiline-strings.php
@@ -1,44 +1,45 @@
 <?php
-function jsonMultilineStringsSplit ($data) {
-  foreach ($data as $k => $v) {
-    if (is_string($v) && strpos($v, "\n") !== false) {
-      $data[$k] = explode("\n", $v);
+function jsonMultilineStringsSplit($data)
+{
+    foreach ($data as $k => $v) {
+        if (is_string($v) && strpos($v, "\n") !== false) {
+            $data[$k] = explode("\n", $v);
+        } elseif (is_array($v)) {
+            $data[$k] = jsonMultilineStringsSplit($v);
+        }
     }
-    elseif (is_array($v)) {
-      $data[$k] = jsonMultilineStringsSplit ($v);
-    }
-  }
 
-  return $data;
+    return $data;
 }
 
 /**
  * return true if the array consists only of strings and all elements are
  * incrementally ordered.
  */
-function isStringArray ($arr) {
-  if (array_keys($arr) !== range(0, sizeof($arr) - 1)) {
-    return false;
-  }
-
-  for ($i = 0; $i < sizeof($arr); $i++) {
-    if (!is_string($arr[$i])) {
-      return false;
+function isStringArray($arr)
+{
+    if (array_keys($arr) !== range(0, sizeof($arr) - 1)) {
+        return false;
     }
-  }
 
-  return true;
+    for ($i = 0; $i < sizeof($arr); $i++) {
+        if (!is_string($arr[$i])) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-function jsonMultilineStringsJoin ($data) {
-  foreach ($data as $k => $v) {
-    if (isStringArray($v)) {
-      $data[$k] = implode("\n", $v);
+function jsonMultilineStringsJoin($data)
+{
+    foreach ($data as $k => $v) {
+        if (isStringArray($v)) {
+            $data[$k] = implode("\n", $v);
+        } elseif (is_array($v)) {
+            $data[$k] = jsonMultilineStringsJoin($v);
+        }
     }
-    elseif (is_array($v)) {
-      $data[$k] = jsonMultilineStringsJoin ($v);
-    }
-  }
 
-  return $data;
+    return $data;
 }
